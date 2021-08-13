30e744b10d96d5b075b43b31d6fec5c8caaae1dc
Anuj Verma <anuj.verma20132@gmail.com>
2021-08-13 17:43:38 +0530

    refactoring

--- remove-pkgs.py
+++ remove-pkgs.py
@@ -1,23 +1,33 @@
 import subprocess, os
 
-# check that a recent version of Android is being used
-# as this affects the uninstall command used
-recentVersion = input("Are you using a recent version of Android (~2015 or later)? [Y]es or [n]o: ").lower() in ['y', 'yes', '']
+def main() -> None:
+    # check that a recent version of Android is being used
+    # as this affects the uninstall command used
+    recentVersion = input("Are you using a recent version of Android (~2015 or later)? [Y]es or [n]o: ").lower() in ['y', 'yes', '']
 
-# get the package list
-list_command="adb.exe shell pm list packages"
-result = subprocess.check_output(list_command, shell=True).decode()
+    packages = getPackages()
 
-# process the list
-names = [i.strip() for i in result.strip().split('\n') if i.strip() != '']
-packages = [i.split(':')[1] for i in names]
+    # print out the lsit of packages
+    print('\n'.join(packages))
 
-# print out the lsit of packages
-print('\n'.join(packages))
+    # iterate over the packages
+    for name in packages:
 
-# iterate over the packages
-for name in packages:
+        prompt = getUserChoice(name)
 
+        # if they chose to skip, continue to the next iteration
+        if prompt.lower() in ['s', 'skip', '']:
+            continue
+
+        # if they chose to abort, exit the script
+        elif prompt.lower() in ['a', 'abort']:
+            break
+
+        # if they chose to continue, uninstall the package
+        elif prompt.lower() in ['c', 'continue']:
+            removePackage(recentVersion, name)
+
+def getUserChoice(name) -> str:
     # assume there is no confirmation
     confirmation = False
 
@@ -26,19 +36,24 @@ for name in packages:
     while not confirmation:
         prompt = input(f"\nUninstalling {name}. [C]ontinue, [s]kip, or [a]bort? ") # find out what the user wants to do
         confirmation = (input("Are you sure? [Y]es or [n]o: ").lower() in ['y', 'yes', '']) # ask them to confirm
+    return prompt
 
-    # if they chose to skip, continue to the next iteration
-    if prompt.lower() in ['s', 'skip', '']:
-        continue
+def removePackage(recentVersion, name) -> None:
+    if recentVersion:
+        os.system(f"adb.exe shell pm uninstall -k --user 0 {name}")
+            
+    else:
+        os.system(f"adb.exe shell pm block {name}")
 
-    # if they chose to abort, exit the script
-    elif prompt.lower() in ['a', 'abort']:
-        break
+def getPackages() -> list:
+    # get the package list
+    list_command="adb.exe shell pm list packages"
+    result = subprocess.check_output(list_command, shell=True).decode()
 
-    # if they chose to continue, uninstall the package
-    elif prompt.lower() in ['c', 'continue']:
-        if recentVersion:
-            os.system(f"adb.exe shell pm uninstall -k --user 0 {name}")
-        
-        else:
-            os.system(f"adb.exe shell pm block {name}")
+    # process the list
+    names = [i.strip() for i in result.strip().split('\n') if i.strip() != '']
+    packages = [i.split(':')[1] for i in names]
+    return packages
+
+if __name__ == "__main__":
+    main()
